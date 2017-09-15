147dec4f1fc68c8b967955b841777adbb207f070
Howard Jing <howard.jing@gmail.com>
2017-09-14 22:44:17 -0400

    add arrow controls and skip to first / last

--- src/game/history.js
+++ src/game/history.js
@@ -13,6 +13,21 @@ const isFirst = (index: number): boolean => index === -1;
 const isLast = <T>(history: T[], index: number) => index >= history.length - 1;
 
 class History extends Component<Props> {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress = (e: KeyboardEvent) => {
+    if (e.key === 'ArrowLeft') {
+      this.goBack();
+    } else if (e.key === 'ArrowRight') {
+      this.goForward();
+    }
+  }
 
   goBack = () => {
     const { index, onChangeIndex } = this.props;
@@ -26,6 +41,16 @@ class History extends Component<Props> {
     onChangeIndex(index + 1);
   };
 
+  goFirst = () => {
+    const { onChangeIndex } = this.props;
+    onChangeIndex(-1);
+  }
+
+  goLast = () => {
+    const { history, onChangeIndex } = this.props;
+    onChangeIndex(history.length - 1);
+  }
+
   render() {
     const { history, index } = this.props;
     return (
@@ -33,12 +58,28 @@ class History extends Component<Props> {
         <Controls>
           <Control
             active={!isFirst(index)}
+            onClick={this.goFirst}
+          >
+            first
+          </Control>
+          <Control
+            active={!isFirst(index)}
             onClick={this.goBack}
-          >&#xab; prev</Control>
+          >
+            &#xab; prev
+          </Control>
           <Control
             active={!isLast(history, index)}
             onClick={this.goForward}
-          >next &#xbb;</Control>
+          >
+            next &#xbb;
+          </Control>
+          <Control
+            active={!isLast(history, index)}
+            onClick={this.goLast}
+          >
+            last
+          </Control>
         </Controls>
       </div>
     );
