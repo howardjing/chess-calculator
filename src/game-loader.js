64b4a4dd9aef00128bb9e128f9e14e2d00443063
Howard Jing <howard.jing@gmail.com>
2017-09-24 22:46:54 -0400

    add ability to load game via pgn query param

--- src/game-loader.js
+++ src/game-loader.js
@@ -3,16 +3,26 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Chess from 'chess.js';
 
+const loadGame = (value: string): ?Chess => {
+  const chess = new Chess();
+
+  if (chess.load_pgn(value)) {
+    return chess;
+  }
+
+  return null;
+}
 class GameLoader extends Component<{
   onNewGame: (chess: Chess) => any,
 }, {}> {
   attemptToLoad = (e: SyntheticInputEvent<*>) => {
     const { value } = e.target;
-
-    const chess = new Chess();
-    if (chess.load_pgn(value)) {
-      const { onNewGame } = this.props;
-      onNewGame(chess);
+    const chess = loadGame(value);
+    if (chess) {
+      if (chess.load_pgn(value)) {
+        const { onNewGame } = this.props;
+        onNewGame(chess);
+      }
     }
   };
   render() {
@@ -31,4 +41,6 @@ const TextArea = styled.textarea`
   width: 100%;
   height: 50px;
 `;
+
 export default GameLoader;
+export { loadGame };
